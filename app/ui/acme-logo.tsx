2845b5bb00aa4400951b3df8edfbb54bd1b76d0a
Roger Hu <roger.hu@ampmservice.com>
2025-02-20 14:03:29 -0800

    change the logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -6,8 +6,12 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <Square3Stack3DIcon className="h-10 w-10 rotate-[-15deg]" />
-      <p className="text-[22px]">AM/PM Service</p>
+      <Square3Stack3DIcon className="h-10 w-10 rotate-[-15deg] mr-2" />
+      <div>
+        <p className="text-[16px]">AM/PM Service</p>
+        <p className="text-[22px] mt-1">Vendor Portal</p>
+      </div>
+      
     </div>
   );
 }
